2a6b6cc1c30d3f2454e46cc7f031974614c8ef23
David Figueroa <davidfigueroa0327@gmail.com>
2020-11-17 21:55:25 -0500

    Workspaces eliminados

--- configuration/tags/init.lua
+++ configuration/tags/init.lua
@@ -11,12 +11,6 @@ local tags = {
     screen = 1
   },
   {
-    icon = icons.code,
-    type = 'code',
-    defaultApp = apps.default.editor,
-    screen = 1
-  },
-  {
     icon = icons.social,
     type = 'social',
     defaultApp = apps.default.social,
@@ -25,12 +19,6 @@ local tags = {
   {
     icon = icons.game,
     type = 'game',
-    defaultApp = apps.default.game,
-    screen = 1
-  },
-  {
-    icon = icons.vm,
-    type = 'vms',
     defaultApp = apps.default.game,
     screen = 1
   },
@@ -44,12 +32,6 @@ local tags = {
     icon = icons.music,
     type = 'music',
     defaultApp = apps.default.music,
-    screen = 1
-  },
-  {
-    icon = icons.lab,
-    type = 'any',
-    defaultApp = apps.default.rofi,
     screen = 1
   }
 }
